fa2a578c0851449b084c70d7c7d3ad84be2fc4e7
Bagus <25834188+contactjavas@users.noreply.github.com>
2023-05-20 11:31:53 +0700

    part of previous commit

--- charts_flutter/lib/src/behaviors/legend/datum_legend.dart
+++ charts_flutter/lib/src/behaviors/legend/datum_legend.dart
@@ -25,8 +25,7 @@ import 'package:charts_common/common.dart' as common
         OutsideJustification,
         SelectionModelType,
         TextStyleSpec;
-import 'package:flutter/widgets.dart'
-    show BuildContext, EdgeInsets, Widget, hashValues;
+import 'package:flutter/widgets.dart' show BuildContext, EdgeInsets, Widget;
 import 'package:meta/meta.dart' show immutable;
 import '../../chart_container.dart' show ChartContainerRenderObject;
 import '../chart_behavior.dart'
